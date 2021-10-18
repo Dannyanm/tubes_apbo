c7f290d03c16e8ae9c4058a4ea690c66d4d0a258
unknown <snder12nr@gmail.com>
2021-10-18 15:05:14 +0700

    latest update

---
+++ app/Views/admin_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN PAGE</title>
+</head>
+<body>
+
+    <h1>ADMIN PAGE</h1>
+
+    <ul>
+        <li><a href="">Data Sparepart</a></li>
+        <li><a href="">Data Booking</a></li>
+        <li><a href="">Data Service</a></li>
+        <li><a href="">Data Penjualan</a></li>
+        <li><a href="">Data Pembayaran</a></li> <!-- View Bukti Bayar -->
+        <li><a href="">Data Mekanik</a></li>
+        <li><a href="">Data Barang</a></li> <!-- Mengelola Data Pengadaan Barang --> 
+        <li><a href="">Grafik</a></li>
+    </ul>
+    
+</body>
+</html>
+
+
