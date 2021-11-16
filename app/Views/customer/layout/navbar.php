5325ea2ff8b36152ab122cd5800d43aefe44fc29
unknown <snder12nr@gmail.com>
2021-11-16 19:54:50 +0700

    update navbar and sidebar

--- app/Views/customer/layout/navbar.php
+++ app/Views/customer/layout/navbar.php
@@ -1,6 +1,6 @@
 <!-- sidebar -->
-<div class="border-end bg-white" id="sidebar-wrapper">
-        <div class="sidebar-heading border-bottom bg-light">
+<div class="border-end" id="sidebar-wrapper">
+        <div class="sidebar-heading border-bottom">
             <a href="<?= base_url('/');?>">Dadang Cornering</a>
         </div>
         <div class="list-group list-group-flush">
@@ -14,16 +14,16 @@
     <!-- page content wrapper -->
     <div id="page-content-wrapper">
         <!-- Top navigation / navbar / header -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <nav class="navbar navbar-expand-lg border-bottom">
             <div class="container-fluid">
-                <button class="btn btn-primary" id="sidebarToggle">///</button>
+                <button class="button-toggler button-toggler-primary" id="sidebarToggle">///</button>
 
                 <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="collapse-toggler-icon"></span>
                 </button> -->
 
                 <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $user_status; ?></a>
+                    <a class="user-option dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $user_status; ?></a>
                     <div class="dropdown-menu dropdown-menu-end pr-8" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="<?= base_url('/customer/profil');?>">Profil</a>
                         <a class="dropdown-item" href="#!">Logout</a>
